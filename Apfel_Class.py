fc456cdf325fd02af0d214547320da1e3f6eb17a
VictorRotha <Victor.Rotha@t-online.de>
2019-12-08 18:43:49 +0100

    save img, parameters

--- Apfel_Class.py
+++ Apfel_Class.py
@@ -1,24 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 class Mandelbrot:
-    def __init__(self):
-        self.limiter = 2
-        self.max_iter = 100
+    def __init__(self, limiter=2, max_iter=100, screen_size=(600,600), center=(-0.5+0.5j), step=0.001):
+        self.limiter = limiter
+        self.max_iter = max_iter
+        w, h = self.width, self.height = screen_size
+        c = self.center = center
+        self.step = step
 
-        w, h = self.width, self.height = 600, 600
-        centerx, centery = self.center = (-0.6, 0.5)
-        self.step = 0.001
-        self.xrange = (centerx - w//2*self.step, centerx + w//2*self.step)
-        self.yrange = (centery - h//2*self.step, centery + h//2*self.step)
+        self.xrange = (c.real - w//2*self.step, c.real + w//2*self.step)
+        self.yrange = (c.imag - h//2*self.step, c.imag + h//2*self.step)
 
         self.screen_array = np.zeros((self.height, self.width))
+
+    def print_parameters(self):
+        print(f'Aufloesung: {self.width}*{self.height} Pixel')
+        print(f'Zentrum: {self.center}\nPixelbreite: {self.step}')
+        print(f'xrange: {self.xrange}, yrange: {self.yrange}')
+        print(f'Limiter: {self.limiter}\nMax. Iterationstiefe: {self.max_iter}')
 
     def create_array(self):
         xmin, xmax = self.xrange
         ymin, ymax = self.yrange
         step = self.step
         max_iter, limiter = self.max_iter, self.limiter
+
         for i in range(self.height):
             for j in range(self.width):
                 c = complex(xmin + step * j, ymax - step * i)
@@ -34,12 +42,30 @@ class Mandelbrot:
                 # n ist Anzahl der Iterationen, bis z > limiter.
                 # Wenn z NICHT über limiter geht (= Mandelbrot Bedingung!!), dann ist n = max_iter
 
-    def show(self):
+    def show(self, colormap=plt.cm.hot_r):
         # title = f'Mandelbrotmenge\nx={self.xrange}, y={self.yrange}, limit={self.limiter}, iter={self.max_iter}\n' \
         #         f'size={self.width}*{self.height}'
-        title = f'Mandelbrotmenge\ncenter={self.center}, px={self.step}\nlimit={self.limiter}, iter={self.max_iter}'
+        title = f'Mandelbrotmenge\ncenter={self.center}, px={self.step}\nlimit={self.limiter}, iter={self.max_iter}\n' \
+                f'Colormap={colormap}'
         plt.title(title)
         plt.xticks([])
         plt.yticks([])
-        plt.imshow(np.log(self.screen_array), cmap=plt.cm.hot_r)
+        plt.imshow(np.log(self.screen_array), cmap=colormap)
         plt.show()
+
+    def save(self, filename, info=False):
+        # TODO check filename/path exits
+        filename = os.path.join(os.getcwd(), '_img', filename)
+        plt.imsave(filename, np.log(self.screen_array), cmap=plt.cm.hot_r)
+        print(f'img saved:\n{filename}')
+        if info:
+            infofile = os.path.splitext(filename)[0] + '.txt'
+            with open(infofile, 'w') as f:
+                f.write(f'Aufloesung: {self.width}*{self.height} Pixel\n\n')
+                f.write(f'Zentrum: {self.center}\nPixelbreite: {self.step}\n')
+                f.write(f'xrange: {self.xrange}, yrange: {self.yrange}\n\n')
+                f.write(f'Limiter: {self.limiter}\nMax. Iterationstiefe: {self.max_iter}')
+
+            print(infofile)
+
+
--- Apfel_Main.py
+++ Apfel_Main.py
@@ -3,8 +3,23 @@ import time
 
 now = time.time()
 
-apfel = Mandelbrot()
+apfel = Mandelbrot(screen_size=(600,600),
+                   center = complex(-0.6,0.5),
+                   step=0.0005)
+apfel.print_parameters()
 apfel.create_array()
 print(time.time()-now)
 apfel.show()
+# apfel.save('mandel01.png', info=True)
+while True:
+    value = input('q=quit, 1=save, 2=show: ')
+    if value.lower() == 'q':
+        break
+    elif value=='2':
+        apfel.show()
+    elif value=='1':
+        filename = input('filename: ')
+        apfel.save(filename, info=True)
+        break
+
 
